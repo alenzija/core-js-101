02aea00f7d463960c52f49b5e2a830361d8e96f4
Alena <alenzija@gmail.com>
2023-05-31 13:46:15 +0200

    feat: add 06-promises tasks solutions

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -28,8 +28,18 @@
  *      .catch((error) => console.log(error.message)) // 'Error: Wrong parameter is passed!
  *                                                    //  Ask her again.';
  */
-function willYouMarryMe(/* isPositiveAnswer */) {
-  throw new Error('Not implemented');
+function willYouMarryMe(isPositiveAnswer) {
+  return new Promise((resolve, reject) => {
+    if (isPositiveAnswer !== undefined && (typeof isPositiveAnswer === 'boolean' || isPositiveAnswer instanceof Boolean)) {
+      if (isPositiveAnswer) {
+        resolve('Hooray!!! She said "Yes"!');
+      } else {
+        resolve('Oh no, she said "No".');
+      }
+    } else {
+      reject(new Error('Wrong parameter is passed! Ask her again.'));
+    }
+  });
 }
 
 
@@ -48,8 +58,8 @@ function willYouMarryMe(/* isPositiveAnswer */) {
  *    })
  *
  */
-function processAllPromises(/* array */) {
-  throw new Error('Not implemented');
+function processAllPromises(array) {
+  return Promise.all(array);
 }
 
 /**
@@ -71,8 +81,8 @@ function processAllPromises(/* array */) {
  *    })
  *
  */
-function getFastestPromise(/* array */) {
-  throw new Error('Not implemented');
+function getFastestPromise(array) {
+  return Promise.race(array);
 }
 
 /**
@@ -92,9 +102,21 @@ function getFastestPromise(/* array */) {
  *    });
  *
  */
-function chainPromises(/* array, action */) {
-  throw new Error('Not implemented');
+function chainPromises(array, action) {
+  const asyncAction = async (a, b) => {
+    const paramB = await b;
+    if (!a) return paramB;
+    return action(paramB, a);
+  };
+
+  if (array.length === 1) {
+    return Promise.resolve(array.pop()).catch(() => Promise.resolve(null));
+  }
+  return Promise.resolve(array.pop())
+    .catch(() => Promise.resolve(null))
+    .then((response) => asyncAction(response, chainPromises(array, action)));
 }
+
 
 module.exports = {
   willYouMarryMe,
